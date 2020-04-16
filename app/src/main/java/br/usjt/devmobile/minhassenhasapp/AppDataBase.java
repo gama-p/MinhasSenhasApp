127200ce6ed458d8bcad3cb28d04e9064d0da346
gama-p <gama.p@outlook.com>
2020-04-16 14:51:04 -0300

    Validação de Campos e Detalhes de Senha

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/AppDataBase.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/AppDataBase.java
@@ -4,6 +4,6 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Senha.class}, version = 1)
-abstract class AppDatabase extends RoomDatabase {
+public abstract class AppDatabase extends RoomDatabase {
     public abstract SenhaDao senhaDao();
 }
